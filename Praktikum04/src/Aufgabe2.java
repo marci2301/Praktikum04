7b1d56431a88358f71e3b307cf68d46372f6a4cf
mmueller23 <mmueller23@7QMHG52.ad.fh-bielefeld.de>
2018-12-18 10:42:25 +0100

    Kommentare hinzugefügt

--- Praktikum04/src/Aufgabe2.java
+++ Praktikum04/src/Aufgabe2.java
@@ -3,21 +3,26 @@ public class Aufgabe2 {
 
 	public static void main(String[] args) {
 		
+		//Eingabe Größe des Arrays
 		StdOut.println("Wie gross soll das Array sein?");
 		int n = StdIn.readInt();
 		
+		//Deklaration und Initialiserung des Arrays
 		double [] darray = new double[n];
 		
+		//for-schleife um jeden Wert vom Benutzer eingeben zu lassen
 		for(int i=0; i<darray.length; i++){
 			StdOut.println("Geben Sie die " + (i+1) + ". Zahl ein: ");
 			int a = StdIn.readInt();
 			darray[i] = a;
 		}
 		
+		//Initialiserung und Deklaration der Variablen
 		double min = darray[0];
 		double max = darray[0];
 		double mittelwert = 0;
 		
+		//"Methode" für Min und Max wert, mittelwert --> werte aufaddieren
 		for(int i=0; i<darray.length; i++){
 			
 			if(min>darray[i]) {
@@ -30,7 +35,10 @@ public class Aufgabe2 {
 			mittelwert += darray[i];
 		}
 		
+		//Berechnung Mittelwert durch die Arraylänge
 		mittelwert = mittelwert/darray.length;
+		
+		//Ausgabe der Werte 
 		StdOut.println("Maximalwert: " + max + " Minimalwert: " + min + " Mittelwert: " + mittelwert);
 	}
 
--- Praktikum04/src/Aufgabe3.java
+++ Praktikum04/src/Aufgabe3.java
@@ -3,22 +3,26 @@ public class Aufgabe3 {
 
 	public static void main(String[] args) {
 		
+		//Eingabe Größe des Arrays
 		StdOut.println("Wie gross soll das Array sein?");
 		int i = StdIn.readInt();
 		
+		//Initialisierung und Deklaration des Arrays
 		int[][] array = new int [i][i];
 		
+		//Verschachtelte Schleife einmal für die Reihe und dann Spalte
 		for(int reihe = 0; reihe <array.length; reihe++) {
 			for(int spalte = 0; spalte < array.length; spalte++) {
-				if(reihe == spalte) {
+				if(reihe == spalte) {      								//wenn spalte = zeile soll er die entsprechende zahl reinschreiben, +1 da im array ab 0 gezählt wird
 					array[reihe][spalte] = reihe+1;
 				}
 				else {
-					array[reihe][spalte] = 0;
+					array[reihe][spalte] = 0;							//sonst immer eine 0 speichern
 				}
 			}
 		}
 		
+		//Ausgabe des Arrays mittels for-schleife
 		for(int reihe = 0; reihe <array.length; reihe++) {
 			for(int spalte = 0; spalte < array.length; spalte++) {
 				StdOut.print(array[reihe][spalte] + " ");
